3996c0414430e438aa15dcb3cce4f8d3285533b1
Hasan <250149@newuu.uz>
2026-02-06 14:48:23 +0500

    First Java project on laptop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
-    public static void main() {
-        System.out.println("Hello World!");
-        System.out.println("Hello Java!");
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int heads = 0;
+        int tails = 0;
+        for(int i = 1; i <= 100; i++){
+            boolean con = random.nextBoolean();
+            if (con){
+                heads++;
+            }
+            else {
+                tails++;
+            }
+        }
+        System.out.println("Heads: " + heads);
+        System.out.println("Tails: " + tails);
+        scanner.close();
     }
 }
